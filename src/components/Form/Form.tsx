2cb2c978fa660a90127241ff23cab0ff1aa7ad30
Victor.G <victorgranda1992@gmail.com>
2023-02-13 23:06:28 +0100

    Add form component and render at App

---
+++ src/components/Form/Form.tsx
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+
+const Form = () => {
+  const [todos, setTodos] = useState([]);
+
+  const handleChange = (event: any) => {
+    setTodos(event.target.value);
+  };
+
+  return (
+    <form>
+      <div>
+        <label htmlFor="name">
+          <input type="text" onChange={handleChange} value={todos} />
+        </label>
+      </div>
+      <button>Add</button>
+    </form>
+  );
+};
+
+export default Form;
