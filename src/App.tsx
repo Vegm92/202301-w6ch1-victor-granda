fb7bce51c470a02c1e75e23e71bb555a8965bc79
Victor Granda <victorgranda1992@gmail.com>
2023-02-14 02:29:05 +0100

    Add changes to form component and todoSlice

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import "./App.css";
 import Form from "./components/Form/Form";
+import TodoList from "./components/TodoList/TodoList";
 
 function App() {
   return (
     <>
       <h1>Todo RTK</h1>
       <Form />
+      <TodoList />
     </>
   );
 }
