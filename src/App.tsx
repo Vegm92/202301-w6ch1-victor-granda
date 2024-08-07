b39fa24fb45afd26923db94c8315d45a312d1972
Victor Granda <victorgranda1992@gmail.com>
2024-08-07 17:17:41 +0200

    Add Task history

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
+import React from "react";
 import Form from "./components/Form/Form";
 import TodoList from "./components/TodoList/TodoList";
+import TaskHistory from "./components/TaskHistory/TaskHistory";
+import { useAppSelector } from "./store/hooks";
 
 function App() {
+  const todos = useAppSelector((state) => state.todos.todos);
+
   return (
     <>
       <h1>Todo RTK</h1>
       <Form />
-      <TodoList />
+      <TodoList todos={todos} />
+      <TaskHistory />
     </>
   );
 }
